baacf0119ca244cf7ae3b0313c0f0c689fec783c
TRFN <tulio.nasc95@gmail.com>
2020-09-24 08:15:54 -0300

    Versão 1.0

---
+++ app/controles/main.php
@@ -0,0 +1,11 @@
+<?php
+    function ctrl_main($ctx){
+        $ctx->regVarStrict("cortema", "#007BFF");
+        $ctx->regVarStrict("corpadrao1", "#F2E527");
+        $ctx->regVarStrict("hue-fundo", "100");
+        $ctx->regVarStrict("brilho-fundo", "80");
+        $ctx->regVarStrict("sat-fundo", "6");
+        $ctx->regVarStrict("m-titulo", "RS Baterias - Grande Variadade e Qualidade em Baterias! (31) 9 9360-3242");
+        $ctx->regVarStrict("m-desc", "RS Baterias - Grande Variadade e Qualidade em Baterias! (31) 9 9360-3242");
+    }
+?>
